e2f8d8fe6196565f4538bcbff1ec26df0e2c8c35
DaveAstator <omnispark@gmail.com>
2025-10-18 15:26:23 +0200

    and fix the script

--- RayLua/script.lua
+++ RayLua/script.lua
@@ -1,23 +1,10 @@
-script.lua
--- Quad position
-quad_x = quad_x or 100
-quad_y = quad_y or 100
-
 function Render()
-    -- Update quad position to follow mouse
-    quad_x = GetMouseX() - 25
-    quad_y = GetMouseY() - 25
-    
-    -- Draw quad
-    DrawRectangle(quad_x, quad_y, 50, 50)
+    DrawRectangle(100, 100, 200, 150)
 end
 
 function RenderUI()
-    ImGuiBegin("Controls")
-    ImGuiText("Mouse X: " .. GetMouseX())
-    ImGuiText("Mouse Y: " .. GetMouseY())
-    ImGuiText("Quad X: " .. quad_x)
-    ImGuiText("Quad Y: " .. quad_y)
-    ImGuiText("Press R to reload script")
+    ImGuiBegin("Test Window")
+    ImGuiText("Hello from Lua!")
+    ImGuiText("Mouse: " .. GetMouseX() .. ", " .. GetMouseY())
     ImGuiEnd()
 end
