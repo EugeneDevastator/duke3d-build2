31a2bc732385c3caec5aa473a7471217d2e5f37c
DaveAstator <omnispark@gmail.com>
2026-09-12 19:36:28 +0200

    comment about the format

--- RayLua/Core/mapserial.h
+++ RayLua/Core/mapserial.h
@@ -9,3 +9,18 @@ int map_save_b2(const char *path, mapstate_t *map);
 int map_load_b2(const char *path, mapstate_t *map);
 
 #endif
+
+/*
+note, sept 2026:
+make it chunked:
+// -- fundamental markup, most stable ---
+sector[] : nwalls, id 
+walls[] : xy, nporals, nwallid[nportals], ownid //mapped to sector 1-1 by nwalls
+xfrom[] : transform, sectorid, ownid // xforms of the sprites with id. basic markup
+// -- attached data, volataile ----
+capart[] // floors and ceils of each sector
+wallart[] //
+spritedata[] : spriteid, data..
+sprlightdata[] : spriteid, data..
+
+*/
